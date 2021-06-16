513f8676cddaeadfd9314a81dccbd49db1f5a2ef
Lisa Pessin <lisapessin@gmail.com>
2021-06-16 17:38:07 -0400

    added apiRoutes, homepage not populating

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,12 +3,61 @@ const db = require("../models");
 module.exports = function(app) {
 
     // app.get - find most recent workout to continue
+    app.get("/api/workouts/", (req, res) => {
+        console.log(req.body);
+
+        db.Workout.find({})
+        .then(lastWorkout => {
+            res.json(lastWorkout);
+        })
+        .catch(error => {
+            res.json(error);
+        });    
+    });
 
     // app.post - create new workout
+    app.post("/api/workouts/", (req, res) => {
+        console.log(req.body);
+
+        db.Workout.create(req.body)
+        .then(newWorkout => {
+            res.json(newWorkout);
+        })
+        .catch(error => {
+            res.json(error);
+        });    
+    });
 
     // app.put - add new exercise to most recent workout plan
+    app.put("/api/workouts/:id", (req, res) => {
+        console.log(req.body);
 
-    // app.put - add new exercise to new workout plan
+        db.Workout.findOneAndUpdate(
+            req.params.id,
+            { $push: { exercises: req.body } },
+            { new: true }
+        )
+        .then(updateWorkout => {
+            res.json(updateWorkout);
+        })
+        .catch(error => {
+            res.json(error);
+        });
+    });
+
+    // /api/workouts/range
+    app.get("/api/workouts/range", (req, res) => {
+        console.log(req.body);
+
+        db.Workout.find({})
+        .limit(7)
+        .then(rangeWorkout => {
+            res.json(rangeWorkout);
+        })
+        .catch(error => {
+            res.json(error);
+        });    
+    });
 
     // app.get - find combined weight from past seven workouts
 
--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const app = express();
-
 const db = require("./models");
+const app = express();
 
 app.use(logger("dev"));
 
@@ -15,8 +14,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// require("./routes/htmlRoutes.js")(app);
-// require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes.js")(app);
+require("./routes/apiRoutes.js")(app);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessdb", { useNewUrlParser: true });
 
