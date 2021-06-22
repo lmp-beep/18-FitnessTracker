80c7aa67218c27a7ea960aa6f00c144668894af6
Lisa Pessin <lisapessin@gmail.com>
2021-06-22 12:42:49 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 const db = require("./models");
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
